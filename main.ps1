ccc635ac273d3555912924a60a8db6e7e3f2182d
Diw Thawatchai Buachan <57717272+diwwergg@users.noreply.github.com>
2026-01-14 10:48:19 +0000

    Add files via upload

--- main.ps1
+++ main.ps1
@@ -11,8 +11,8 @@ if (-not (Get-Module -Name PSReadLine)) {
     Install-Module -Name PSReadLine -Force -SkipPublisherCheck
     Import-Module -Name PSReadLine
 }
-Import-Module "${rootMainPath}\module\lsdeluxe.ps1"
-Import-Module "${rootMainPath}\module\ps-readline.ps1"
+# Import-Module "${rootMainPath}\module\lsdeluxe.ps1"
+Import-Module "${rootMainPath}\module\eza.ps1"
 Import-Module "${rootMainPath}\module\ps-readline.ps1"
 Import-Module "${rootMainPath}\module\fnm.ps1"
 Import-Module "${rootMainPath}\utility\shortcut-open.ps1"
@@ -21,7 +21,7 @@ Import-Module "${rootMainPath}\utility\shortcut-open.ps1"
 $previousOutputEncoding = [Console]::OutputEncoding
 [Console]::OutputEncoding = [Text.Encoding]::UTF8
 try {
-    oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\space.omp.json" | Invoke-Expression
+    oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\amro.omp.json" | Invoke-Expression
 }
 finally {
     [Console]::OutputEncoding = $previousOutputEncoding
